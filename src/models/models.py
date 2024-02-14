886b5f0f5bcbe04a851995b6c3c52a2cf2ba50e3
silvering-steve <sstevenadi@gmail.com>
2024-02-14 21:03:59 +0700

    update model download

--- src/models/models.py
+++ src/models/models.py
@@ -1,11 +1,24 @@
+import os
 from typing import Tuple, Any
 
+import wget
 from ultralytics import YOLO
 
 
 class Model:
     def __init__(self) -> None:
-        self.model = YOLO("models/bubbles.onnx", task="detect")
+        if not os.path.exists("models/bubbles.onnx"):
+            wget.download(
+                url="https://silveringtrialstorage.blob.core.windows.net/trial-container/models/detection/onnx/bubbles.onnx",
+                out="./models"
+            )
+
+        try:
+            self.model = YOLO("models/bubbles.onnx", task="detect")
+
+            print("Model loaded!")
+        except:
+            raise 'Error when loading model'
 
     def __call__(self, *args, **kwargs) -> tuple[bool, Any] | None:
         result = self.model(kwargs["image"], conf=0.82, verbose=False, imgsz=256)
